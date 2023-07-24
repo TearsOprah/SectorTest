b50bffb2d469ad867cbc19cca48c154fc5c2a3cb
TearsOprah <qmeshokp@mail.ru>
2023-07-24 12:47:27 +0300

    feat: получение постов

--- src/stores/PostStore.js
+++ src/stores/PostStore.js
@@ -26,7 +26,20 @@ class PostStore {
       currentData: computed,
       totalPages: computed,
     });
+
+    this.fetchPosts().then(() => {});
   }
+
+  // Метод для получения данных с сервера
+  fetchPosts = async () => {
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+      const data = await response.json();
+      this.setPosts(data);
+    } catch (error) {
+      console.error('Error fetching posts:', error);
+    }
+  };
 
   setPosts(posts) {
     this.posts = posts;
