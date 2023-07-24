b00ae4b88fac7aee045e3fc53cddd7026edee9c1
TearsOprah <qmeshokp@mail.ru>
2023-07-24 14:33:28 +0300

    feat: поиск по кнопке

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Search.css';
 
 const Search = ({ searchTerm, onSearch }) => {
-  const handleSearch = (event) => {
-    onSearch(event.target.value);
+  const [searchValue, setSearchValue] = useState(searchTerm);
+
+  const handleInputChange = (event) => {
+    setSearchValue(event.target.value);
+  };
+
+  const handleSearch = () => {
+    onSearch(searchValue);
   };
 
   return (
     <div className="search">
       <input
         type="text"
-        value={searchTerm}
-        onChange={handleSearch}
+        value={searchValue}
+        onChange={handleInputChange}
         placeholder="Search"
       />
+      <button onClick={handleSearch}>Поиск</button>
     </div>
   );
 };
